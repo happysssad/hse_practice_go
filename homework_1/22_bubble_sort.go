d94ac51e5c42b06d7a23303096efb3be0a759e9c
Maria Deminova <mashademinova1@gmail.com>
2024-07-06 23:28:00 +0300

    Create 22_bubble_sort.go
    Задача№ 22.
    Сортировка пузырьком - Реализуйте алгоритм сортировки пузырьком для массива целых чисел.

---
+++ homework_1/22_bubble_sort.go
@@ -0,0 +1,11 @@
+func BubbleSort(array []int) []int {
+	n := len(array)
+	for i := 0; i < n; i++ {
+		for j := 0; j < n-1; j++ {
+			if array[j] > array[j+1] {
+				array[j], array[j+1] = array[j+1], array[j]
+
+			}
+		}
+	}
+}
